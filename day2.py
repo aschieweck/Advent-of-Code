cbe720f82d4311702d85d11c5c6000cda6c66f01
Alexander Schieweck <alexander.schieweck@web.de>
2025-12-02 21:03:55 +0000

    2025 - Day 2: Performance tests

--- day2.py
+++ day2.py
@@ -1,49 +1,104 @@
+import cProfile
+import re
 import sys
 
 
-def check_ids(file_name: str) -> int:
+def read_file(file_name: str) -> str:
+    with open(file_name) as f:
+        return f.read().splitlines()[0]
+
+
+def check_ids(input: str) -> int:
     result = 0
 
-    with open(file_name) as f:
-        line = f.readline().strip()
-        id_ranges = line.split(",")
+    id_ranges = input.split(",")
 
-        for id_range in id_ranges:
-            ids = id_range.split("-")
-            id1 = int(ids[0])
-            id2 = int(ids[1])
+    for id_range in id_ranges:
+        ids = id_range.split("-")
+        id1 = int(ids[0])
+        id2 = int(ids[1])
 
-            for i in range(id1, id2 + 1):
-                i_str = str(i)
-                split_idx = len(i_str) // 2
-                if i_str[:split_idx] == i_str[split_idx:]:
+        for i in range(id1, id2 + 1):
+            i_str = str(i)
+            split_idx = len(i_str) // 2
+            if i_str[:split_idx] == i_str[split_idx:]:
+                result += i
+
+    return result
+
+
+def check_ids_regex(input: str) -> int:
+    result = 0
+
+    id_ranges = input.split(",")
+
+    regex = re.compile(r'^(\d+)\1$')
+
+    for id_range in id_ranges:
+        ids = id_range.split("-")
+        id1 = int(ids[0])
+        id2 = int(ids[1])
+
+        for i in range(id1, id2 + 1):
+            i_str = str(i)
+
+            if regex.match(i_str):
+                result += i
+
+    return result
+
+
+def check_ids_2(input: str) -> int:
+    result = 0
+
+    id_ranges = input.split(",")
+
+    for id_range in id_ranges:
+        ids = id_range.split("-")
+        id1 = int(ids[0])
+        id2 = int(ids[1])
+
+        for i in range(id1, id2 + 1):
+            i_str = str(i)
+
+            for j in range(1, len(i_str) // 2 + 1):
+                chunks = [i_str[k:k + j] for k in range(0, len(i_str), j)]
+                if len(set(chunks)) == 1:
                     result += i
+                    # early exit of chunking as it's an silly id already
+                    break
 
     return result
 
 
-def check_ids_2(file_name: str) -> int:
+def check_ids_2_regex(input: str) -> int:
     result = 0
 
-    with open(file_name) as f:
-        line = f.readline().strip()
-        id_ranges = line.split(",")
+    id_ranges = input.split(",")
 
-        for id_range in id_ranges:
-            ids = id_range.split("-")
-            id1 = int(ids[0])
-            id2 = int(ids[1])
+    regex = re.compile(r'^(\d+)\1+$')
 
-            for i in range(id1, id2 + 1):
-                i_str = str(i)
+    for id_range in id_ranges:
+        ids = id_range.split("-")
+        id1 = int(ids[0])
+        id2 = int(ids[1])
 
-                for j in range(1, len(i_str) // 2 + 1):
-                    chunks = [i_str[k:k + j] for k in range(0, len(i_str), j)]
-                    if len(set(chunks)) == 1:
-                        result += i
-                        # early exit of chunking as it's an silly id already
-                        break
+        for i in range(id1, id2 + 1):
+            i_str = str(i)
+
+            if regex.match(i_str):
+                result += i
+
     return result
+
+
+def profile_and_run(label, func, arg):
+    profiler = cProfile.Profile()
+    profiler.enable()
+    retval = func(arg)
+    profiler.disable()
+    print(f"{label}:\t", retval)
+    profiler.print_stats()
 
 
 if __name__ == "__main__":
@@ -51,5 +106,9 @@ if __name__ == "__main__":
         print("Please provide the input file!", file=sys.stderr)
         exit(-1)
 
-    print("Phase 1:", check_ids(sys.argv[1]))
-    print("Phase 2:", check_ids_2(sys.argv[1]))
+    input = read_file(sys.argv[1])
+
+    profile_and_run("Phase 1", check_ids, input)
+    profile_and_run("Phase 1 (regex)", check_ids_regex, input)
+    profile_and_run("Phase 2", check_ids_2, input)
+    profile_and_run("Phase 2 (regex)", check_ids_2_regex, input)
